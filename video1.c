f5e1e6dbb64ffb666f7b27a34eb2a6796b127b30
Carina Cruz <lecruz@bu.edu>
2018-06-16 15:22:21 +0000

    Using an object/class member types

--- video1.c
+++ video1.c
@@ -16,3 +16,18 @@ public class Person
 Person person = new Person();   //(type, identifyier) = (new operator,name of class=person) //we're initializing an instance of the class
 
 var person = new Person();  //shorter way to write the above. var is a c sharp keyword. We use var to keep us from typing the full name of a class+ to avoid duplication
+
+//Using an object
+var person = new Person();  //object
+person.Name = "Mosh"; (//field name + value)
+person.Introduce();  //method
+
+//class members: Instance vs Static/
+//Instance member is accessible from an object. Ex below
+
+var person = new Person();
+person.Introduce();
+
+//A static member is accessible from the class
+//Why use static members: To represent concepts that are singleton(meaning we should only have one instance of the concept in the memory, DateTime.Now, and Console.WriteLine()
+Console.WriteLine();  //console is the class, writeline is the static method
