ea6d43bfd9873c9eb8ff3563801fa87d6a46c229
Carina Cruz <lecruz@bu.edu>
2018-06-16 14:48:37 +0000

    declaring a class

--- video1.c
+++ video1.c
@@ -1,2 +1,11 @@
 //work from Mosh's C Sharp Intermediate course to go here//
 
+public class Person
+{
+    public string Name;  //field, starting with an access modifyer (public) and data tyoe(string) + identifier (Name)
+
+    public void Introduct()  //method, return type void)doesn't return
+    {
+        Console.WriteLine("Hi, my name is " + Name); //displaying name of person on console
+    }
+}
