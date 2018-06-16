c1d40637f56a1e89d285394f809660262d370c30
Carina Cruz <lecruz@bu.edu>
2018-06-16 15:06:42 +0000

    creating an object

--- video1.c
+++ video1.c
@@ -1,5 +1,6 @@
 //work from Mosh's C Sharp Intermediate course to go here//
 
+//declaring a class
 public class Person
 {
     public string Name;  //field, starting with an access modifyer (public) and data tyoe(string) + identifier (Name)
@@ -9,3 +10,9 @@ public class Person
         Console.WriteLine("Hi, my name is " + Name); //displaying name of person on console
     }
 }
+
+//creating an object
+
+Person person = new Person();   //(type, identifyier) = (new operator,name of class=person) //we're initializing an instance of the class
+
+var person = new Person();  //shorter way to write the above. var is a c sharp keyword. We use var to keep us from typing the full name of a class+ to avoid duplication
