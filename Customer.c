feb38ed90d07f9b91fb4c4e1906edba292e9f210
Carina Cruz <lecruz@bu.edu>
2018-06-17 23:57:58 +0000

    constructor

--- Customer.c
+++ Customer.c
@@ -17,3 +17,19 @@ namespace CSharpIntermediate
             Name = "unknown";
         }
 
+
+ // Constructor that takes one argument:
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        // Auto-implemented readonly property:
+        public string Name { get; }
+
+        // Method that overrides the base class (System.Object) implementation.
+        public override string ToString()
+        {
+            return Name.ToString();
+        }
+    }
