0d1a1c4214fe4b778019e475f90e2bb05c412bc5
Carina Cruz <lecruz@bu.edu>
2018-06-18 23:59:20 +0000

    update

--- Customer.c
+++ Customer.c
@@ -53,3 +53,17 @@ namespace CSharpIntermediate
             Console.ReadKey();
         }
     }
+
+______
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+    //Other properties, methods, events...
+}
