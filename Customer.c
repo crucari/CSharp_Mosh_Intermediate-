d4dd756fbc884e5e4f0bdd359e7f0e24a8a44a88
Carina Cruz <lecruz@bu.edu>
2018-06-19 23:38:16 +0000

    tuesday update

--- Customer.c
+++ Customer.c
@@ -5,6 +5,21 @@ namespace CSharpIntermediate
         public int Id;
         public string Name;
 
+        public Customer()
+        {
+
+        }
+
+        public Customer(int id)
+        {
+            this.Id = id;
+        }
+
+        public Customer(int id, string name)
+        {
+            this.Id - id;
+            this.Name = name;
+        }
     }
 }
 
--- Program.c
+++ Program.c
@@ -1,10 +1,21 @@
 namespace CSharpIntermediate
 {
+public class Order
+{
+
+}
     class Program
     {
         static void Main(string[] args)
         {
-            var customer = new Customer();
+            // var customer = new Customer(1, "John");
+
+        var customer = new customer();
+        customer.Id = 1;
+        customer.Name = "John",
+
+        Console.WriteLine(customer.Id);
+        Console.WriteLine(customer.Name);
 
         }
     }
