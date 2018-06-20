212fd42223cbe6805e0a4059731bb53040cee57e
Carina Cruz <lecruz@bu.edu>
2018-06-20 23:49:19 +0000

    program2

---
+++ Program2.c
@@ -0,0 +1,11 @@
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var point = new Point(10, 20);
+            point.Move(new Point(40,60));
+        }
+    }
+}
