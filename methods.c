3b41a9bf0c0ec9cd0c66628571c55cd656a4bd6e
Carina Cruz <lecruz@bu.edu>
2018-06-20 23:40:26 +0000

    update

--- methods.c
+++ methods.c
@@ -88,5 +88,20 @@ myClass.MyMethod(out a);
 
 namespace Methods
 {
+     public class Point
+     {
+         public int X;
+         public int Y;
 
+         public Point(int x, int y)
+         {
+             this.X = z;
+             this.Y = y;
+         }
+
+         public void Move(int x, int y)
+         {
+
+         }
+     }
 }
