a1bf71f709658fa7abeb7b69d91cdf2c51df4eda
Carina Cruz <lecruz@bu.edu>
2018-06-20 23:45:25 +0000

    update

---
+++ point.c
@@ -0,0 +1,20 @@
+namespace Methods
+{
+     public class Point
+     {
+         public int X;
+         public int Y;
+
+         public Point(int x, int y)
+         {
+             this.X = z;
+             this.Y = y;
+         }
+
+         public void Move(int x, int y)
+         {
+            this.X = x;
+            this.Y = y;
+         }
+     }
+}
