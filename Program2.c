6f392c840203297e07edf201f8d8dd3a66c8c702
Carina Cruz <lecruz@bu.edu>
2018-06-20 23:50:43 +0000

    program2 update

--- Program2.c
+++ Program2.c
@@ -6,6 +6,9 @@ namespace Methods
         {
             var point = new Point(10, 20);
             point.Move(new Point(40,60));
+            console.WriteLine("Point is at ({0}, {1})"), point.X, point Y);
+            
+            point.Move(100, 200);
         }
     }
 }
