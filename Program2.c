2746e08ae12ec0bdead1d27368c4403c2ea53f40
Carina Cruz <lecruz@bu.edu>
2018-06-21 22:30:29 +0000

    thurs update

--- Program2.c
+++ Program2.c
@@ -1,14 +1,28 @@
 namespace Methods
+// {
+//     class Program
+//     {
+//         static void Main(string[] args)
+//         {
+//             var point = new Point(10, 20);
+//             point.Move(new Point(40,60));
+//             console.WriteLine("Point is at ({0}, {1})"), point.X, point Y);
+
+//             point.Move(100, 200);
+//         }
+//     }
+// }
+
+----
+
+class Program
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var point = new Point(10, 20);
-            point.Move(new Point(40,60));
-            console.WriteLine("Point is at ({0}, {1})"), point.X, point Y);
-            
-            point.Move(100, 200);
-        }
-    }
+    static void Main(string[] args)
+
+}
+
+try
+{
+    var point = new Point(10,20);
+    point.Move(null);
 }
