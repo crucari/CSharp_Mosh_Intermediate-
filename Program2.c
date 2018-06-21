2de705512662310a89ab0566c6d91b75956b7591
Carina Cruz <lecruz@bu.edu>
2018-06-21 22:32:49 +0000

    update

--- Program2.c
+++ Program2.c
@@ -25,4 +25,11 @@ try
 {
     var point = new Point(10,20);
     point.Move(null);
+    console.WriteLine("Point is at ({0}, {1})"), point.X, point Y);
+
+    point.Move(100, 200);
+    Console.WriteLine("Point is at ({0}, {1})"), point.X, point Y);
+
 }
+
+// }
